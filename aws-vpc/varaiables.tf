bb0c08d54486f8b6a1a644ec704c7060fea37fa1
cloudiehubio-spec <cloudiehub.io@gmail.com>
2025-09-15 05:57:12 -0700

    Update varaiables.tf

--- aws-vpc/varaiables.tf
+++ aws-vpc/varaiables.tf
@@ -1,1 +1,36 @@
+variable "aws_region" {
+  type        = string
+  description = "AWS region to deploy into"
+  default     = "us-east-1"
+}
+
+variable "vpc_cidr" {
+  type        = string
+  description = "CIDR for the VPC"
+  default     = "10.0.0.0/16"
+}
+
+variable "public_subnet_cidrs" {
+  type        = list(string)
+  description = "List of public subnet CIDRs"
+  default     = ["10.0.1.0/24", "10.0.2.0/24"]
+}
+
+variable "private_subnet_cidrs" {
+  type        = list(string)
+  description = "List of private subnet CIDRs"
+  default     = ["10.0.11.0/24", "10.0.12.0/24"]
+}
+
+variable "enable_nat_gateway" {
+  type        = bool
+  description = "If true, create NAT Gateways in each AZ for private subnets to reach internet"
+  default     = true
+}
+
+variable "tags" {
+  type        = map(string)
+  description = "Tags to apply to resources"
+  default     = { "Project" = "cloudiehub" }
+}
 
