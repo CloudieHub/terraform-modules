5e59b27f26d33e9fcbefa4eab54eefd18fd5a992
cloudiehubio-spec <cloudiehub.io@gmail.com>
2025-09-15 05:59:32 -0700

    Update outputs.tf

--- aws-vpc/outputs.tf
+++ aws-vpc/outputs.tf
@@ -1,1 +1,20 @@
+output "vpc_id" {
+  value       = aws_vpc.this.id
+  description = "VPC id"
+}
+
+output "public_subnet_ids" {
+  value       = values(aws_subnet.public).*id
+  description = "List of public subnet ids"
+}
+
+output "private_subnet_ids" {
+  value       = values(aws_subnet.private).*id
+  description = "List of private subnet ids"
+}
+
+output "nat_gateway_ids" {
+  value       = var.enable_nat_gateway ? aws_nat_gateway.nat.*.id : []
+  description = "NAT Gateway ids (if enabled)"
+}
 
