1b828a38d6aad47dccf0a20aee493a5974e3f325
cloudiehubio-spec <cloudiehub.io@gmail.com>
2025-10-01 23:18:10 -0700

    Create main.tf

---
+++ Azure/azure-storage-account/main.tf
@@ -0,0 +1,34 @@
+terraform {
+  required_version = ">= 1.2.0"
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = ">= 3.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "this" {
+  name     = var.rg_name
+  location = var.location
+}
+
+resource "azurerm_storage_account" "this" {
+  name                     = var.storage_account_name
+  resource_group_name      = azurerm_resource_group.this.name
+  location                 = azurerm_resource_group.this.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+
+  tags = var.tags
+}
+
+resource "azurerm_storage_container" "this" {
+  name                  = var.container_name
+  storage_account_name  = azurerm_storage_account.this.name
+  container_access_type = "private"
+}
