65a496210900380baf85374696d4ec248267fa5b
cloudiehubio-spec <cloudiehub.io@gmail.com>
2025-10-02 00:04:08 -0700

    Create main.tf

---
+++ GCP/gcp-storage-bucket/main.tf
@@ -0,0 +1,14 @@
+resource "google_storage_bucket" "this" {
+  name          = var.bucket_name
+  location      = var.location
+  force_destroy = var.force_destroy
+  storage_class = var.storage_class
+
+  versioning {
+    enabled = var.versioning
+  }
+
+  uniform_bucket_level_access = var.uniform_bucket_level_access
+
+  labels = var.labels
+}
